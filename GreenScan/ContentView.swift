971a8b4fabbc14da28929d5fb410e2ab3cb10a65
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-03-04 17:49:18 +0100

    Initial Commit

---
+++ GreenScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreenScan
+//
+//  Created by Francesco Sallia on 04.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreenScan/GreenScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenScanApp.swift
+//  GreenScan
+//
+//  Created by Francesco Sallia on 04.03.25.
+//
+
+import SwiftUI
+
+@main
+struct GreenScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
